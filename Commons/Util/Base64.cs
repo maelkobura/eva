2c4b9c0621bbbb57ef8e5419ab53b01561b74660
Maël Kobura <62505405+maelkobura@users.noreply.github.com>
2026-03-01 17:18:24 +0100

    add Base64 utility class

---
+++ Commons/Util/Base64.cs
@@ -0,0 +1,17 @@
+﻿namespace Eva.Commons.Util;
+
+public class Base64
+{
+    public static string Base64UrlEncode(byte[] data) =>
+        Convert.ToBase64String(data)
+            .TrimEnd('=')
+            .Replace('+', '-')
+            .Replace('/', '_');
+
+    public static byte[] Base64UrlDecode(string data)
+    {
+        var s = data.Replace('-', '+').Replace('_', '/');
+        s = s.PadRight(s.Length + (4 - s.Length % 4) % 4, '=');
+        return Convert.FromBase64String(s);
+    }
+}
