526693966f66253424a7fb2dc1e0a1ce6ff2b65b
Maël Kobura <62505405+maelkobura@users.noreply.github.com>
2026-03-01 17:18:44 +0100

    add Certificate entity for validation

---
+++ Commons/Security/Certificate/CertificateEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Eva.Commons.Security.Certificate;
+
+public record CertificateEntity(string Name, CertificateType Type, string[] Authorization, long Expiration);
+
+public enum CertificateType {
+    User,
+    Node
+}
