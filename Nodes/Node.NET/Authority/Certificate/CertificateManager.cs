fda508af4574a7d7b8aa50b1dadaff1f829caf76
Maël Kobura <62505405+maelkobura@users.noreply.github.com>
2026-03-15 17:47:27 +0100

    add CertificateManager

---
+++ Nodes/Node.NET/Authority/Certificate/CertificateManager.cs
@@ -0,0 +1,41 @@
+﻿using Eva.Commons.Security.Certificate;
+using Eva.Commons.Util;
+using Microsoft.Extensions.Logging;
+
+namespace Eva.Node.Authority.Certificate;
+
+public class CertificateManager
+{
+    public static CertificateManager? Instance { get; private set; }
+    private static ILogger logger = EvaLogger.CreateLogger<CertificateManager>();
+
+    public static void Init(string token)
+    {
+        if (Instance != null) return;
+        Instance = new CertificateManager(token);
+    }
+
+    private readonly string token;
+    private string certificateRaw;
+    private CertificateEntity certificate;
+    
+    private CertificateManager(string token)
+    {
+        this.token = token;
+    }
+    
+    public void GenerateCertificate()
+    {
+        if (certificateRaw == null)
+        {
+            logger.LogInformation("Creating node certificate...");
+        }
+        else
+        {
+            logger.LogInformation("Refreshing node certificate...");
+        }
+        
+        //TODO Fetch EAS
+    }
+    
+}
