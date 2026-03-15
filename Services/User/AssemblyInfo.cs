73a578bb1dce6e5b4b5cc37ca37001476c91c613
Maël Kobura <62505405+maelkobura@users.noreply.github.com>
2026-03-15 17:10:54 +0100

    move service.yml data to AssemblyInfo.cs and authorization.txt

---
+++ Services/User/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyMetadata("Name", "user")]
+[assembly: AssemblyMetadata("DisplayName", "User Service")]
+[assembly: AssemblyMetadata("Description", "Provides User context and information to Eva Network")]
+[assembly: AssemblyMetadata("Version", "1.0.0")]
+[assembly: AssemblyMetadata("Author", "Maël Kobura")]
+[assembly: AssemblyMetadata("License", "MIT")]
+[assembly: AssemblyMetadata("Class", "Eva.Services.User.UserService")]
+[assembly: AssemblyMetadata("Authorization", "Eva.Services.User.authorization.txt")]
