36ec3f3ac8b84dd15f3f2e1a2d82b5dfaa75e4fc
Maël Kobura <62505405+maelkobura@users.noreply.github.com>
2026-03-15 15:46:16 +0100

    add Node.NET configuration

--- Nodes/Node.NET/Program.cs
+++ Nodes/Node.NET/Program.cs
@@ -1,13 +1,28 @@
-﻿using Eva.Commons.Util;
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using Eva.AuthorityServer.System;
+using Eva.Commons.Util;
 using Microsoft.Extensions.Logging;
 using Mono.Options;
 
 string nodeConfigPath = "node.yml";
+string serviceBinaryPath = "node.dll";
+var configOverride = new Dictionary<string, string>();
 
 var options = new OptionSet {
-    { "nc|nodeconfig=", "Node config path", n => nodeConfigPath = n }
+    { "nc|nodeconfig=", "Node config path", n => nodeConfigPath = n },
+    { "b|bin=", "Eva Service binary path", n => serviceBinaryPath = n },
+    { "p|prop=:", "Property key value", (key, value) =>
+        {
+            configOverride[key] = value;
+        }
+    }
 };
 
 EvaLogger.Init("Node");
 var log = EvaLogger.CreateLogger<Program>();
 log.LogInformation("Initializing Eva Node (.NET)...");
+log.LogInformation("Current Runtime: {}", RuntimeInformation.FrameworkDescription);
+using var templateStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Eva.Node.node.default.yml");
+
+Configuration.Init(nodeConfigPath, configOverride, templateStream);
