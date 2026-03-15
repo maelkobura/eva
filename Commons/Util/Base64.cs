3bba287da73d3008b9198481c4122d8745d7479f
Maël Kobura <62505405+maelkobura@users.noreply.github.com>
2026-03-15 16:32:43 +0100

    add random token generation to Base64 class

--- Commons/Util/Base64.cs
+++ Commons/Util/Base64.cs
@@ -1,4 +1,6 @@
-﻿namespace Eva.Commons.Util;
+﻿using System.Security.Cryptography;
+
+namespace Eva.Commons.Util;
 
 public class Base64
 {
@@ -13,5 +15,16 @@ public class Base64
         var s = data.Replace('-', '+').Replace('_', '/');
         s = s.PadRight(s.Length + (4 - s.Length % 4) % 4, '=');
         return Convert.FromBase64String(s);
+    }
+    
+    public static string GenerateToken(int length = 32)
+    {
+        byte[] bytes = new byte[length];
+        using (var rng = RandomNumberGenerator.Create())
+        {
+            rng.GetBytes(bytes);
+        }
+        string token = Convert.ToBase64String(bytes);
+        return token.Substring(0, length);
     }
 }
