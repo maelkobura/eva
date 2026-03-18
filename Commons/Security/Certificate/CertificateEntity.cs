465dfe5eca40649f103cad3bc69d15da7b5312f9
Maël Kobura <62505405+maelkobura@users.noreply.github.com>
2026-03-18 19:30:32 +0100

    remove old cert managing

--- Commons/Security/Certificate/CertificateEntity.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Eva.Commons.Security.Certificate;
-
-public record CertificateEntity(string Name, string UniqueId, CertificateType Type, string[] Authorization, long Expiration, bool AuthorityToken);
-
-public enum CertificateType {
-    User,
-    Node
-}
