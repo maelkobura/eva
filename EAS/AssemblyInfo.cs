843fee51dca09e351e6c5515eea3f0f7c3bdd347
Maël Kobura <62505405+maelkobura@users.noreply.github.com>
2026-03-18 16:57:50 +0100

    adding immuable eas version information in assembly metadata

---
+++ EAS/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyMetadata("Name", "eascore")]
+[assembly: AssemblyMetadata("Version", "1.0.0")]
+[assembly: AssemblyMetadata("Version Info", "prerelease")]
+[assembly: AssemblyMetadata("Author", "Maël Kobura")]
+[assembly: AssemblyMetadata("URL", "https://github.com/maelkobura/eva")]
