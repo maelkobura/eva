8ee254e0d1ff4ca2dd57398363adf26220ddd42d
Maël Kobura <62505405+maelkobura@users.noreply.github.com>
2026-03-18 20:28:38 +0100

    creating EvaEngine base

---
+++ Services/EvaEngine/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyMetadata("Name", "evaengine")]
+[assembly: AssemblyMetadata("DisplayName", "EvaEngine")]
+[assembly: AssemblyMetadata("Description", "Core of Eva Conversation System & Script Engine")]
+[assembly: AssemblyMetadata("Version", "1.0.0")]
+[assembly: AssemblyMetadata("Author", "Maël Kobura")]
+[assembly: AssemblyMetadata("License", "MIT")]
+[assembly: AssemblyMetadata("Class", "Eva.Services.Engine.EvaEngine")]
+[assembly: AssemblyMetadata("Authorization", "Eva.Services.Engine.authorization.txt")]
