04825dfe3e604b966569df46d29f4b96dbd0b91e
Maël Kobura <62505405+maelkobura@users.noreply.github.com>
2026-03-27 19:56:05 +0100

    add EntityManager

---
+++ Nodes/Node.NET/Network/EntityManager.cs
@@ -0,0 +1,20 @@
+﻿using Eva.Commons.Util;
+using Microsoft.Extensions.Logging;
+
+namespace Eva.Node.Network;
+
+public class EntityManager
+{
+    public static EntityManager? Instance { get; private set; }
+    private static ILogger logger = EvaLogger.CreateLogger<EntityManager>();
+
+    public List<NodeEntity> Nodes { get; private set; } = new List<NodeEntity>();
+    
+    public static void Init()
+    {
+        if (Instance != null) return;
+        Instance = new EntityManager();
+    }
+
+    
+}
