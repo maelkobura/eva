becb6eafe2d584acb823c6346520b30067a50e44
Maël Kobura <62505405+maelkobura@users.noreply.github.com>
2026-03-28 22:24:06 +0100

    add methods to generate a protobuf panel for function

--- Nodes/Node.NET/Service/Functions/FunctionRegistry.cs
+++ Nodes/Node.NET/Service/Functions/FunctionRegistry.cs
@@ -1,5 +1,7 @@
 ﻿using System.Reflection;
 using Eva.Commons.Util;
+using Eva.Commons.Messages;
+using Google.Protobuf.Collections;
 using Microsoft.Extensions.Logging;
 
 namespace Eva.Node.Service.Functions;
@@ -96,5 +98,74 @@ public class FunctionRegistry
             return type.GetGenericArguments()[0];
         return type;
     }
+    
+    public FunctionPanel GetPanel()
+    {
+        
+        List<EvaFunctionDescriptor> descs = new();
+
+        foreach (var descriptor in _functions.Values)
+        {
+            var evadesc = new EvaFunctionDescriptor()
+            {
+                Id = descriptor.Id,
+                Name =  descriptor.Name,
+                Description = descriptor.Description,
+                ReturnType = MapType(descriptor.ReturnType)
+            };
+            
+            evadesc.Keywords.Add(descriptor.Keywords);
+            evadesc.Authorization.Add(descriptor.Authorization);
+
+            foreach (var param in descriptor.Parameters)
+            {
+                evadesc.Parameters.Add(new EvaParameterDescriptor
+                {
+                    Name =  param.Name,
+                    Type = MapType(param.Type),
+                    IsRequired = param.IsRequired
+                });
+            }
+            
+            descs.Add(evadesc);
+        }
+
+        var panel = new FunctionPanel
+        {
+            ServiceId = ServiceLoader.Instance!.ServiceId.ToSnakeCase(),
+        };
+        panel.Functions.Add(descs);
+        return panel;
+    }
+    
+    private static ReturnType MapType(Type type)
+    {
+        if (type.IsArray)
+        {
+            var elementType = type.GetElementType()!;
+            return new ReturnType { Type = EvaType.Array, ArrayType = MapPrimitive(elementType) };
+        }
+
+        if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(List<>))
+        {
+            var elementType = type.GetGenericArguments()[0];
+            return new ReturnType { Type = EvaType.Array, ArrayType = MapPrimitive(elementType) };
+        }
+
+        return new ReturnType { Type = MapPrimitive(type) };
+    }
+
+    private static EvaType MapPrimitive(Type type) => type switch
+    {
+        _ when type == typeof(string)   => EvaType.String,
+        _ when type == typeof(int)      => EvaType.Int32,
+        _ when type == typeof(long)     => EvaType.Int64,
+        _ when type == typeof(bool)     => EvaType.Boolean,
+        _ when type == typeof(float)    => EvaType.Float,
+        _ when type == typeof(double)   => EvaType.Double,
+        _ when type == typeof(DateTime) => EvaType.Timestamp,
+        _ when type == typeof(byte[])   => EvaType.Bytes,
+        _                               => EvaType.Object
+    };
 }
     
