1473b9f76b98466623364622357734d54a975d53
Maël Kobura <62505405+maelkobura@users.noreply.github.com>
2026-03-28 13:06:33 +0100

    add UserService and EvaEngine main class

---
+++ Services/EvaEngine/EvaEngine.cs
@@ -0,0 +1,20 @@
+﻿using Eva.Commons.Util;
+using Eva.Node.Service;
+using Microsoft.Extensions.Logging;
+
+namespace Eva.Services.Engine;
+
+public class EvaEngine : EvaService {
+    
+    private static ILogger logger = EvaLogger.CreateLogger<EvaEngine>();
+    
+    public void Initialize()
+    {
+        logger.LogInformation("Hello from EvaEngine");
+    }
+
+    public void Shutdown()
+    {
+        logger.LogInformation("Goodbye from EvaEngine");
+    }
+}
--- Services/User/UserService.cs
+++ Services/User/UserService.cs
@@ -1,6 +1,20 @@
-﻿namespace Eva.Services.User;
+﻿using Eva.Commons.Util;
+using Eva.Node.Service;
+using Microsoft.Extensions.Logging;
 
-public class UserService
-{
+namespace Eva.Services.User;
+
+public class UserService : EvaService {
     
+    private static ILogger logger = EvaLogger.CreateLogger<UserService>();
+    
+    public void Initialize()
+    {
+        logger.LogInformation("Hello from UserService");
+    }
+
+    public void Shutdown()
+    {
+        logger.LogInformation("Goodbye from UserService");
+    }
 }
