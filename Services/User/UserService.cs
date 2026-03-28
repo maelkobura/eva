086ffbb255a28db6dd625d89d3300b795f2b7b70
Maël Kobura <62505405+maelkobura@users.noreply.github.com>
2026-03-28 21:16:02 +0100

    add some test EvaFunction in UserService

--- Services/User/UserService.cs
+++ Services/User/UserService.cs
@@ -1,5 +1,6 @@
 ﻿using Eva.Commons.Util;
 using Eva.Node.Service;
+using Eva.Node.Service.Functions;
 using Microsoft.Extensions.Logging;
 
 namespace Eva.Services.User;
@@ -11,10 +12,20 @@ public class UserService : EvaService {
     public void Initialize()
     {
         logger.LogInformation("Hello from UserService");
+        FunctionRegistry.Instance!.RegisterLambda("update_user_data", "Update user data", new[] { "user", "data" }, () =>
+        {
+            //Do nothing
+        });
     }
 
     public void Shutdown()
     {
         logger.LogInformation("Goodbye from UserService");
+    }
+
+    [EvaFunction(Description = "Get user data", Keywords = new[] { "user", "data" })]
+    public void GetUserData()
+    {
+        //Do nothing for now
     }
 }
