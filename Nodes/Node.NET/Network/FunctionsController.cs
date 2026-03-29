7c39e36a1a41a02b648789ab160837d5023a5fcc
Maël Kobura <62505405+maelkobura@users.noreply.github.com>
2026-03-29 16:23:35 +0200

    add invoke endpoint

--- Nodes/Node.NET/Network/FunctionsController.cs
+++ Nodes/Node.NET/Network/FunctionsController.cs
@@ -1,6 +1,8 @@
 ﻿using EmbedIO;
 using EmbedIO.Routing;
 using EmbedIO.WebApi;
+using Eva.Commons.Messages;
+using Eva.Commons.Security.Certificate;
 using Eva.Node.Service.Functions;
 using Google.Protobuf;
 
@@ -9,9 +11,57 @@ namespace Eva.Node.Network;
 public class FunctionsController : WebApiController{
     
     [Route(HttpVerbs.Get, "/")]
-    public object GetFunctions()
+    public async Task GetFunctions()
     {
-        return Convert.ToBase64String(FunctionRegistry.Instance!.GetPanel().ToByteArray());
+        var panel = FunctionRegistry.Instance.GetPanel();
+        var bytes = panel.ToByteArray();
+        HttpContext.Response.ContentType = "application/x-protobuf";
+        HttpContext.Response.ContentLength64 = bytes.Length;
+        await HttpContext.Response.OutputStream.WriteAsync(bytes);
     }
+    
+    [Route(HttpVerbs.Get, "/{name}")]
+    public async Task GetFunction(string name)
+    {
+        var reg = FunctionRegistry.Instance!;
+        var func = reg.Get(name);
+        if(func == null) throw new Exception("Function not found");
+        var responseBytes = reg.GetDescriptor(func).ToByteArray();
+        HttpContext.Response.ContentType = "application/x-protobuf";
+        HttpContext.Response.ContentLength64 = responseBytes.Length;
+        await HttpContext.Response.OutputStream.WriteAsync(responseBytes);
+    }
+    [Route(HttpVerbs.Post, "/{name}")]
+    public async Task InvokeFunction(string name)
+    {
+        // Read raw bytes from body
+        using var ms = new MemoryStream();
+        await HttpContext.Request.InputStream.CopyToAsync(ms);
+        var requestBytes = ms.ToArray();
+
+        // Deserialize InvokeRequest from protobuf
+        var request = InvokeRequest.Parser.ParseFrom(requestBytes);
+
+        var cert = HttpContext.GetItem<Certificate>("certificate");
+
+        // Create executor
+        var executor = FunctionRegistry.Instance.CreateExecutor(name);
+        if (executor is null)
+        {
+            await HttpContext.SendStandardHtmlAsync(404);
+            return;
+        }
+
+        // Execute
+        var response = await executor.ExecuteAsync(request, cert);
+
+        // Serialize response to protobuf and send
+        var responseBytes = response.ToByteArray();
+        HttpContext.Response.ContentType = "application/x-protobuf";
+        HttpContext.Response.ContentLength64 = responseBytes.Length;
+        await HttpContext.Response.OutputStream.WriteAsync(responseBytes);
+    }
+    
+    
     
 }
