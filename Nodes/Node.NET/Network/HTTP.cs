4766416b750ca607ec8f4a4a8be0691bc2cd6c01
Maël Kobura <62505405+maelkobura@users.noreply.github.com>
2026-03-29 20:27:42 +0200

    add http util for create tls client

---
+++ Nodes/Node.NET/Network/HTTP.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Eva.Node.Authority.Certificate;
+
+namespace Eva.Node.Network;
+
+public class HTTP
+{
+    public static HttpClient CreateHttpClient(out bool secure)
+    {
+        secure = false;
+        var handler = new HttpClientHandler();
+
+        if (CertificateManager.Instance!.TlsEasCertificate is not null)
+        {
+            handler.ServerCertificateCustomValidationCallback = (_, cert, chain, errors) =>
+            {
+                chain!.ChainPolicy.TrustMode = X509ChainTrustMode.CustomRootTrust;
+                chain.ChainPolicy.CustomTrustStore.Add(CertificateManager.Instance.TlsEasCertificate);
+                return chain.Build(new X509Certificate2(cert!));
+            };
+            secure = true;
+        }
+
+        return new HttpClient(handler);
+    }
+}
