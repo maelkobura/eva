e2fd7d231fe9c6d4356492afd560944761a35177
Maël Kobura <62505405+maelkobura@users.noreply.github.com>
2026-04-05 17:47:39 +0200

    add config for engine

---
+++ Services/EvaEngine/EngineConfiguration.cs
@@ -0,0 +1,6 @@
+﻿namespace Eva.Services.Engine;
+
+public class EngineConfiguration
+{
+    public string AssistantName { get; set; } = "Eva";
+}
--- Services/EvaEngine/EvaEngine.cs
+++ Services/EvaEngine/EvaEngine.cs
@@ -1,4 +1,5 @@
 ﻿using Eva.Commons.Util;
+using Eva.Node.Configuration;
 using Eva.Node.Network;
 using Eva.Node.Service;
 using Eva.Node.Service.Functions;
@@ -9,10 +10,13 @@ namespace Eva.Services.Engine;
 public class EvaEngine : EvaService {
     
     private static ILogger logger = EvaLogger.CreateLogger<EvaEngine>();
+
+    [Configurationable]
+    public Configuration<EngineConfiguration> Config;
     
     public void Initialize()
     {
-        logger.LogInformation("Hello from EvaEngine");
+        logger.LogInformation("Hello from EvaEngine (Assistant: " + Config.Value.AssistantName + ")");
     }
 
     public void Shutdown()
@@ -23,6 +27,6 @@ public class EvaEngine : EvaService {
     [EvaFunction(Description = "Prompt Eva", Keywords = new[] { "ai", "runtime" })]
     public string Prompt(string username)
     {
-        return "[EVA] " + EvaServices.Call<string>("user.get_user_data", username).Result;
+        return "[" + Config.Value.AssistantName + "] " + EvaServices.Call<string>("user.get_user_data", username).Result;
     }
 }
