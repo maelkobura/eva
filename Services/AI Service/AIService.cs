4dae7c669ddf042487fa260fb32891c0a800f1a3
Maël Kobura <62505405+maelkobura@users.noreply.github.com>
2026-04-13 20:54:19 +0200

    add AI Service

---
+++ Services/AI Service/AIService.cs
@@ -0,0 +1,37 @@
+﻿using Eva.Commons.Util;
+using Eva.Drivers.Abstractions.Drivers;
+using Eva.Drivers.Abstractions.Messages;
+using Eva.Drivers.Core;
+using Eva.Drivers.Core.Configuration;
+using Eva.Node.Configuration;
+using Eva.Node.Service;
+using Eva.Services.Engine;
+using Microsoft.Extensions.Logging;
+
+namespace Eva.Services.AI;
+
+public class AIService : EvaService {
+    
+    private static ILogger logger = EvaLogger.CreateLogger<EvaEngine>();
+
+    [Configurationable]
+    public Configuration<DriverConfiguration> Config;
+    
+    private DriverLoader _driverLoader;
+    
+    public void Initialize()
+    {
+        _driverLoader = new DriverLoader(Config.Value);
+        _driverLoader.DriverTypes = new[]
+        {
+            typeof(LargeLanguageModelDriver)
+        };
+        _driverLoader.DriverPath = @"K:\Projets\Blume\Eva\Services\AI Service\bin\Debug\net9.0\drivers";
+        _driverLoader.Load();
+    }
+
+    public void Shutdown()
+    {
+        _driverLoader.Dispose();
+    }
+}
---
+++ Services/AI Service/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyMetadata("Name", "ai")]
+[assembly: AssemblyMetadata("DisplayName", "AI Services")]
+[assembly: AssemblyMetadata("Description", "")]
+[assembly: AssemblyMetadata("Version", "1.0.0")]
+[assembly: AssemblyMetadata("Author", "Maël Kobura")]
+[assembly: AssemblyMetadata("License", "MIT")]
+[assembly: AssemblyMetadata("Class", "Eva.Services.AI.AIService")]
+[assembly: AssemblyMetadata("Authorization", "Eva.Services.AI.authorization.txt")]
